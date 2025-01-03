a1777b0bb97ce051eb55b28c038cab8e67bbea2a
Daniel Sauble <djsauble@gmail.com>
2025-01-02 16:42:51 -0800

    Use GitHub Actions to deploy the app to GitHub Pages

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/life-on-the-run/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
